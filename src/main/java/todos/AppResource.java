c6a215be136979fc94cb6c9ca22dd27a0bb0d8fd
Christian Köberl <derkoe@users.noreply.github.com>
2023-07-28 11:03:32 +0200

    chore: switch to Quarkus 3.x (JakartaEE) (#76)

--- src/main/java/todos/AppResource.java
+++ src/main/java/todos/AppResource.java
@@ -1,9 +1,9 @@
 package todos;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 @Path("/")
 public class AppResource {
